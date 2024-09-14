9f669ac139ddae6a46fe3a918c32c4baa41356c9
Blowser <ig.cortez@duocuc.cl>
2024-09-13 21:36:56 -0300

    Update admin.py

--- tienda_juegos/juegos/admin.py
+++ tienda_juegos/juegos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Producto, Categoria, AñoDesarrollo, Desarrollador, Editor, Restriccion
 
-# Register your models here.
+# Registrar cada modelo en el panel de administración
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(AñoDesarrollo)
+admin.site.register(Desarrollador)
+admin.site.register(Editor)
+admin.site.register(Restriccion)
