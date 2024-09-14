00185efa9a626290a1ce2b687c93b018d975866a
Blowser <ig.cortez@duocuc.cl>
2024-09-13 22:14:31 -0300

    Update views.py

--- tienda_juegos/juegos/views.py
+++ tienda_juegos/juegos/views.py
@@ -1,5 +1,7 @@
 # juegos/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Producto
+from .forms import ProductoForm
 # para el index
 def index(request):
     return render(request, 'juegos/index.html')
@@ -59,13 +61,51 @@ def detalles_rocketleague(request):
 
 # para los formularios
 def registrar_cuenta(request):
-    # Aquí puedes manejar la lógica para el formulario de registro de cuenta
     return render(request, 'juegos/registrarcuenta.html')
 
 def modificar_perfil(request):
-    # Aquí puedes manejar la lógica para el formulario de modificar perfil
     return render(request, 'juegos/modificarperfil.html')
 
 def recuperar_contrasena(request):
-    # Aquí puedes manejar la lógica para el formulario de recuperar contraseña
     return render(request, 'juegos/recuperarcontrasena.html')
+
+
+# para el CRUD
+# Vista para listar productos
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'juegos/listar_productos.html', {'productos': productos})
+
+# Vista para crear un nuevo producto
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'juegos/crear_producto.html', {'form': form})
+
+# Vista para editar un producto
+def editar_producto(request, sku):
+    producto = get_object_or_404(Producto, sku=sku)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'juegos/editar_producto.html', {'form': form, 'producto': producto})
+
+# Vista para eliminar un producto
+def eliminar_producto(request, sku):
+    producto = get_object_or_404(Producto, sku=sku)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'juegos/eliminar_producto.html', {'producto': producto})
+
+
+
