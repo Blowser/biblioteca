0c836c55f03742196471b7782c3b50871a5c5258
Blowser <ig.cortez@duocuc.cl>
2024-09-14 17:18:12 -0300

    Update urls.py

--- tienda_juegos/juegos/urls.py
+++ tienda_juegos/juegos/urls.py
@@ -1,18 +1,32 @@
 # juegos/urls.py
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [#html, luego nombre funcion, finalmente name, el name para llamar en los htmls
                
     path('', views.index, name='index'),
     path('vercatalogo/', views.ver_catalogo, name='ver_catalogo'),
-    path('iniciarsesion/', views.iniciar_sesion, name='iniciar_sesion'),
     path('accion/', views.categoria_accion, name='categoria_accion'),
     path('mundoabierto/', views.categoria_mundo_abierto, name='categoria_mundo_abierto'),
     path('freetoplay/', views.categoria_free_to_play, name='categoria_free_to_play'),
     path('supervivencia/', views.categoria_supervivencia, name='categoria_supervivencia'),
     path('carrerasydeportes/', views.categoria_carreras_deportes, name='categoria_carreras_y_deportes'),
     
+    #paths de login y formularios
+    # Login y autenticación
+    path('iniciarsesion/', views.iniciar_sesion, name='iniciar_sesion'),
+    path('registrarcuenta/', views.registrar_cuenta, name='registrar_cuenta'),
+    path('modificarperfil/', views.modificar_perfil, name='modificar_perfil'),
+    
+    #recuperación de contraseña:
+    path('recuperarcontrasena/', auth_views.PasswordResetView.as_view(template_name='juegos/recuperarcontrasena.html'), name='password_reset'),
+    path('recuperarcontrasena/enviado/', auth_views.PasswordResetDoneView.as_view(template_name='juegos/recuperarcontrasenadone.html'), name='password_reset_done'),
+    path('recuperarcontrasena/confirmar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='recuperarcontrasenaconfirm.html'), name='password_reset_confirm'),
+    path('recuperarcontrasena/completo/', auth_views.PasswordResetCompleteView.as_view(template_name='juegos/recuperarcontrasenacomplete.html'), name='password_reset_complete'),
+    # Página para enviar el correo de recuperación de contraseña
+ 
+
+
     #paths para el catálogo
     path('detalles/elderscroll/', views.detalles_elderscroll, name='detalles_elderscroll'),
     path('detalles/fc24/', views.detalles_fc24, name='detalles_fc24'),
@@ -25,20 +39,9 @@ urlpatterns = [#html, luego nombre funcion, finalmente name, el name para llamar
     path('detalles/eldenring/', views.detalles_eldenring, name='detalles_eldenring'),
     path('detalles/rocketleague/', views.detalles_rocketleague, name='detalles_rocketleague'),
     
-    
-    #paths para los formularios:
-    path('registrarcuenta/', views.registrar_cuenta, name='registrar_cuenta'),
-    path('modificarperfil/', views.modificar_perfil, name='modificar_perfil'),
-    path('recuperarcontrasena/', views.recuperar_contrasena, name='recuperar_contrasena'),
-    
     #paths CRUD
     path('productos/', views.listar_productos, name='listar_productos'),
     path('productos/crear/', views.crear_producto, name='crear_producto'),
     path('productos/editar/<str:sku>/', views.editar_producto, name='editar_producto'),
     path('productos/eliminar/<str:sku>/', views.eliminar_producto, name='eliminar_producto'),
 ]
-
-
-
-
-
