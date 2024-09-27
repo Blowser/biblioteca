4fafb649d5ceb49495e4697cfb007b9053d6e736
Blowser <ig.cortez@duocuc.cl>
2024-09-27 18:16:05 -0300

    Update forms.py
    CREANDO PEDIDOS

--- tienda_juegos/juegos/forms.py
+++ tienda_juegos/juegos/forms.py
@@ -7,3 +7,10 @@ class ProductoForm(forms.ModelForm):
         model = Producto
         fields = ['sku', 'nombre', 'descripcion', 'precio', 'categoria', 'añodesarrollo', 'desarrollador', 'editor', 'restriccion', 'imagen']
 
+from django import forms
+from .models import Pedido
+
+class PedidoForm(forms.ModelForm):
+    class Meta:
+        model = Pedido
+        fields = ['producto', 'cantidad']
